9369eb5d2dd5dcf15751709012cb886ccf7316b2
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-05-27 22:57:08 +0300

    code phase 3

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -6,6 +6,11 @@ class Article:
         
 class Author:
     def __init__(self, name):
+        if not isinstance(name, str):#To ensure the name is a string
+            raise TypeError("The name must be a string")
+        if len(name) == 0:#To check characters of the name 
+            raise ValueError("Type the auther's name")
+        self.name = name
         self.name = name
 
     def articles(self):
@@ -22,8 +27,28 @@ class Author:
 
 class Magazine:
     def __init__(self, name, category):
+        if not isinstance(name, str):
+            raise TypeError("Magazine name must be a string")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("The name should be between 2 and 16 characters long")
+        if not isinstance(category, str):
+            raise TypeError("Magazine category must be a string")
+        if len(category) == 0:
+            raise ValueError("Magazine category must not be empty")
         self.name = name
         self.category = category
+    @property
+    def category(self):
+        return self._category
+    
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Magazine category must be a string")
+        if len(value) == 0:
+            raise ValueError("Magazine category must not be empty")
+        self._category = value
 
     def articles(self):
         pass
